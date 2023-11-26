005cf0d5a10164dc816f0ca57489b76f09c70355
Gift <gift.maputle@ctucareer.co.za>
2023-11-26 20:54:58 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CafeNeo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
